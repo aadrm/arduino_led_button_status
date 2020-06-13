c6ccf7f9423d1293ce31233f03f2125ca3e8c573
aadrm <aadrm@hotmail.com>
2020-06-13 07:42:29 +0000

    reordered lines created by merging branches

--- led_buttons_status.ino
+++ led_buttons_status.ino
@@ -3,11 +3,11 @@
 */
 const int button1 = 1;
 const int button2 = 2;
+const int button3 = 3;
+const int button4 = 4;
 
 const int led1 = 11;
 
-const int button3 = 3;
-const int button4 = 4;
 
 void setup() {
 
